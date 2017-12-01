c6db4e5cea4165feffd05a812508d487f7eda358
Peter <merrygoat@users.noreply.github.com>
2017-12-01 18:46:03 +0000

    imporves structure

--- experimental_g_r.py
+++ experimental_g_r.py
@@ -1,109 +1,73 @@
 #!/usr/bin/env python
-import os
 import scipy as sp
 from scipy import spatial,stats
 import numpy as np
-from numpy import random
 import sys
 import time
+import matplotlib.pyplot as plt
 
-start_time = time.time()
-np.seterr(divide='ignore', invalid='ignore')
-filename= ''
+def main():
+    start_time = time.time()
+    #np.seterr(divide='ignore', invalid='ignore')
 
-print("#----------------------------------------------------------------")
-print("#-----------Tool for determination of G(r)-----------------------")
-print("#----------------------------------------------------------------")
+    # Read in and process arguments
+    if len(sys.argv) != 3:
+        print("ERROR: missing mandatory argument!")
+        print("usage: Gr coordfile  binsize[pixel]")
+        print("example SeriesXX_coords.txt 0.5")
+        exit(2)
+    if len(sys.argv) == 3:
+        filename = sys.argv[1]
+        dr = float(sys.argv[2])
 
-if len(sys.argv)<3:
-    print("ERROR: missing mandatory argument!")
-    print("usage: Gr coordfile  binsize[pixel]")
-    print("example SeriesXX_coords.txt 0.5")
-    exit(11)
-if len(sys.argv) >= 3:
-  filename= sys.argv[1]
-  dr = float(sys.argv[2])
+    print("# file %s" % filename)
+    particle_positions = np.genfromtxt(filename)
+    num_particles = len(particle_positions)
 
+    print("# size before cutting borders: ")
+    print("# x size: %d %d "%(particle_positions[:,0].min(),particle_positions[:,0].max()))
+    print("# y size: %d %d "%(particle_positions[:,1].min(),particle_positions[:,1].max()))
+    print("# z size: %d %d "%(particle_positions[:,2].min(),particle_positions[:,2].max()))
+    print("# Number of Particles: %d " % num_particles)
+    print("# init done %1.3f s" % (time.time() - start_time))
 
+    start_time = time.time()
+    bins=np.arange(0,particle_positions[:,0].max(), dr)
+    dimensions = 3
 
-print("# file %s"%filename)
-C= np.genfromtxt(filename)
+    # create a random gas in the same box as the real particles
+    random_gas_positions = np.zeros((num_particles, dimensions))
+    for i in range(0, dimensions):
+        random_gas_positions[:, i] = np.random.uniform(particle_positions[:,i].min(),particle_positions[:,i].max(),size=num_particles)
 
+    #calculate distances for randomn gas and bin them
+    RID = sp.spatial.distance.pdist(random_gas_positions, 'euclidean').flatten()
+    HID, binsID, binnumbersID = sp.stats.binned_statistic(RID, RID, statistic='count', bins=bins)
+    print("# ideal gas done %1.3f s"%(time.time() - start_time))
 
-print("# size before cutting borders: ")
+    # Calculate distances for experimental particles and bin them
+    start_time = time.time()
+    RC = sp.spatial.distance.pdist(particle_positions, 'euclidean').flatten()
+    print("# particles done %1.3f s"%(time.time() - start_time))
+    print("# bin size: %f array [%f,%f]"%(dr,1,RC.max()))
+    print("# number of bins: %d"%(len(bins)))
+    H,bins,binnumbers=sp.stats.binned_statistic(RC, RC, statistic='count', bins=bins)
 
-print("# x size: %d %d "%(C[:,0].min(),C[:,0].max()))
-print("# y size: %d %d "%(C[:,1].min(),C[:,1].max()))
-print("# z size: %d %d "%(C[:,2].min(),C[:,2].max()))
+    #calculate bin centers and normalise
+    bincenters = 0.5*(bins[1:]+bins[:-1])
+    hist=H/HID
+    #take care of 0/0 and x/0, set to 0
+    hist[np.isnan(hist)] = 0
+    hist[np.isinf(hist)] = 0
+    #save result in file
+    with open(filename+"_CC_r.hist",'wb') as f:
+        np.savetxt(f, np.column_stack((bincenters,hist,H,HID)), fmt='%f')
 
+    print("# binning done %1.3f s"%(time.time() - start_time))
 
+    plt.plot(bincenters[:len(bincenters)],hist[:len(bincenters)],'-')
+    plt.ylim(0,5)
+    plt.xlim(0,150)
+    plt.show()
 
-
-num_particles=len(C)
-
-
-
-print("# Number of Particles: %d "%num_particles)
-print("# init done %1.3f s"%(time.time() - start_time))
-start_time = time.time()
-bins=np.arange(0,C[:,0].max(),dr)
-
-# for i in range(3):
-# print i
-    #create random numbers in the same range as the real particles
-ID0=np.random.uniform(C[:,0].min(),C[:,0].max(),size=len(C))
-ID1=np.random.uniform(C[:,1].min(),C[:,1].max(),size=len(C))
-ID2=np.random.uniform(C[:,2].min(),C[:,2].max(),size=len(C))
-
-
-ID=np.vstack((ID0,ID1))
-ID=(np.vstack((ID,ID2))).T
-
-
-#calculate distances
-RID=sp.spatial.distance.pdist(ID, 'euclidean').flatten()
-
-HID,binsID,binnumbersID=sp.stats.binned_statistic(RID, RID, statistic='count', bins=bins)
-#     try:
-#         IdealPart+=HID
-#     except Exception, e:
-#         IdealPart=HID
-
-
-# HID=IdealPart/3.
-
-
-print("# ideal gas done %1.3f s"%(time.time() - start_time))
-start_time = time.time()
-
-RC=sp.spatial.distance.pdist(C, 'euclidean').flatten()
-
-print("# particles done %1.3f s"%(time.time() - start_time))
-start_time = time.time()
-
-
-
-print("# bin size: %f array [%f,%f]"%(dr,1,RC.max()))
-print("# number of bins: %d"%(len(bins)))
-
-H,bins,binnumbers=sp.stats.binned_statistic(RC, RC, statistic='count', bins=bins)
-
-#calculate bin centers and divide ParticleHist/IdealGasHist
-bincenters = 0.5*(bins[1:]+bins[:-1])
-hist=H/HID
-#take care of 0/0 and x/0, set to 0
-hist[np.isnan(hist)] = 0
-hist[np.isinf(hist)] = 0
-#save result in file
-f=open(filename+"_CC_r.hist",'wb')
-np.savetxt(f, np.column_stack((bincenters,hist,H,HID)), fmt='%f')
-
-f.close()
-
-print("# binning done %1.3f s"%(time.time() - start_time))
-import pylab as pl
-pl.plot(bincenters[:len(bincenters)],hist[:len(bincenters)],'-')
-pl.ylim(0,5)
-pl.xlim(0,150)
-pl.show()
-# exit(0)
+main()
